31850685fa969db833d5dbc6a5a5e21b4b1b0f42
kauã oraboni <70423924+Orabonii@users.noreply.github.com>
2024-05-19 18:04:57 -0300

    Add files via upload

---
+++ projeto07/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'; 
+
+import BuscaDeFilmes from './Components/Projeto07/Projeto07'; 
+import './App.css'
+  
+
+function App() { 
+
+  return ( 
+
+    <div className="App"> 
+
+      <BuscaDeFilmes/> 
+
+    </div> 
+
+  ); 
+
+} 
+
+export default App; 
